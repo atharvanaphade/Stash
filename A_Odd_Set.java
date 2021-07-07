7b2f65bbc9adcb31b865da07a97a80ee90c762e0
Atharva Naphade <atharvanaphade@Atharvas-MacBook-Air.local>
2021-07-07 19:44:42 +0530

    1st Prob

---
+++ A_Odd_Set.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Odd_Set {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc --> 0) {
+            int n = sc.nextInt(), o = 0, e = 0;
+            for (int i = 0; i < 2 * n; i++) {
+                int temp = sc.nextInt();
+                if (temp % 2 == 0) e++;
+                if (temp % 2 == 1) o++;
+            }
+            if (o != e) System.out.println("No");
+            else System.out.println("Yes");
+        }
+    }
+}
